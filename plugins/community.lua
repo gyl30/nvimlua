147cca0af5d7358113bb620cbb2e6633614d21cc
gyl30 <im.ggyyll@gmail.com>
2023-05-22 21:37:38 +0800

    update community

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,8 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  { import = "astrocommunity.colorscheme.dracula" },
+  { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
+  { import = "astrocommunity.indent.indent-tools-nvim" },
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
