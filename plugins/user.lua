9000247c8378b3a4f9a76cb31bd94744a3ea8ea8
gyl <im.ggyyll@gmail.com>
2023-05-23 14:27:08 +0800

    add clangd ex setting

--- plugins/user.lua
+++ plugins/user.lua
@@ -7,14 +7,32 @@ return {
       require("lsp_signature").setup()
     end,
   },
-  "p00f/clangd_extensions.nvim", -- install lsp plugin
+  {
+    "p00f/clangd_extensions.nvim",
+    init = function() table.insert(astronvim.lsp.skip_setup, "clangd") end,
+    ft = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+    opts = function() return { server = require("astronvim.utils.lsp").config "clangd" } end,
+  },
   {
     "williamboman/mason-lspconfig.nvim",
     opts = {
       ensure_installed = { "clangd" }, -- automatically install lsp
     },
+
   },
   { "lvimuser/lsp-inlayhints.nvim",        config = true },
 
   { "lukas-reineke/indent-blankline.nvim", enable = false },
+  {
+    "glepnir/lspsaga.nvim",
+    event = "LspAttach",
+    config = function()
+      require("lspsaga").setup({})
+    end,
+    dependencies = {
+      { "nvim-tree/nvim-web-devicons" },
+      --Please make sure you install markdown and markdown_inline parser
+      { "nvim-treesitter/nvim-treesitter" }
+    }
+  },
 }
