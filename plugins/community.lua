a7d3060673a30e620bd06c86b7185619f42dc28a
gyl <im.ggyyll@gmail.com>
2023-05-23 14:27:21 +0800

    enable community

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,5 +3,13 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.dracula" },
   { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
+  { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
+  { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
+  { import = "astrocommunity.completion.codeium-vim" },
   { import = "astrocommunity.git.git-blame-nvim" },
 }
