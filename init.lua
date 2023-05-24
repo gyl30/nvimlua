9c99320ae8e78be21294db0edf2fc41aff4ded90
gyl <im.ggyyll@gmail.com>
2023-05-24 10:29:47 +0800

    update config

--- init.lua
+++ init.lua
@@ -20,12 +20,6 @@ return {
   -- Set colorscheme to use
   colorscheme = "astrodark",
 
-  -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-  diagnostics = {
-    virtual_text = true,
-    underline = true,
-  },
-
   lsp = {
     config = {
       setup_handlers = {
--- options.lua
+++ options.lua
@@ -2,11 +2,11 @@
 return {
   opt = {
     relativenumber = false, -- sets vim.opt.relativenumber
-    number = true, -- sets vim.opt.number
-    spell = false, -- sets vim.opt.spell
-    signcolumn = "no", -- sets vim.opt.signcolumn to auto
+    number = true,          -- sets vim.opt.number
+    spell = false,          -- sets vim.opt.spell
+    signcolumn = "no",      -- sets vim.opt.signcolumn to auto
     foldcolumn = "0",
-    wrap = false, -- sets vim.opt.wrap
+    wrap = false,           -- sets vim.opt.wrap
     tabstop = 4,
     shiftwidth = 4,
     softtabstop = 4,
@@ -14,20 +14,20 @@ return {
     expandtab = true,
     mouse = "",
     virtualedit = { "block", "onemore" },
-    showtabline = 0, -- don't show tabline
+    showtabline = 2, -- don't show tabline
     foldmethod = "expr",
     foldexpr = "nvim_treesitter#foldexpr()",
     foldlevel = 99,
     foldenable = false,
   },
   g = {
-    mapleader = " ", -- sets vim.g.mapleader
+    mapleader = " ",                 -- sets vim.g.mapleader
     maplocalleader = ";",
-    autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-    cmp_enabled = true, -- enable completion at start
-    autopairs_enabled = true, -- enable autopairs at start
-    diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-    icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+    autoformat_enabled = true,       -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+    cmp_enabled = true,              -- enable completion at start
+    autopairs_enabled = true,        -- enable autopairs at start
+    diagnostics_mode = 3,            -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+    icons_enabled = true,            -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
   },
 }
