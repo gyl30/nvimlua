7227978d8622e542d39bc13ccc32a9acc66679db
gyl30 <im.ggyyll@gmail.com>
2023-06-07 09:05:57 +0800

    update lsp-inlayhints

--- lsp/on_attach.lua
+++ lsp/on_attach.lua
@@ -1,11 +1,4 @@
 return function(client, bufnr)
-    if client.server_capabilities.inlayHintProvider then
-        local inlayhints_avail, inlayhints = pcall(require, "lsp-inlayhints")
-        if inlayhints_avail then
-            inlayhints.on_attach(client, bufnr)
-            vim.keymap.set("n", "<leader>uH", function() inlayhints.toggle() end, { desc = "Toggle inlay hints" })
-        end
-    end
     if client.name == 'gopls' and not client.server_capabilities.semanticTokensProvider then
         local semantic = client.config.capabilities.textDocument.semanticTokens
         client.server_capabilities.semanticTokensProvider = {
